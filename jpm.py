b6f0e596c887b7d4fd34e3590701e5be1c4f636e
Steven Zhang <steven.shutao@gmail.com>
2019-06-10 22:22:14 +0800

    map to geneva position OK

--- jpm.py
+++ jpm.py
@@ -3,15 +3,21 @@
 # Read holdings and cash data from JPM bank statements and convert them into
 # Geneva format.
 #
-from tester.file import itemGroup
+
 from jpm_revised.utility import getCurrentDirectory
-from utils.iter import pop
+from utils.iter import pop, itemGroup, firstOf
 from utils.excel import worksheetToLines
+from investment_lookup.id_lookup import get_investment_Ids, \
+                                        lookup_investment_currency
 from xlrd import open_workbook
 from operator import add
-from functools import reduce
+from functools import reduce, partial
 from itertools import filterfalse, islice, chain
-from functools import partial
+
+
+
+emptyString = lambda x: True if isinstance(x, str) and x.strip() == '' else False
+emptyLine = lambda line: all(emptyString(x) for x in line)
 
 
 
@@ -34,13 +40,12 @@ def readJPM(lines):
     accountLine = lambda L: True if len(L) > 0 and str(L[0]).startswith('Account:') \
                             else False
     sections = itemGroup(accountLine, lines)
-    dateString = readDateFromHeader(pop(sections))   # consume the first section
+    dateString = dateFromHeader(pop(sections))   # consume the first section
     return dateString
 
 
 
-
-def readDateFromHeader(lines):
+def dateFromHeader(lines):
     """
     [Iterable] lines => [String] date
 
@@ -73,21 +78,21 @@ def account(lines):
 
     (1) Normal case:
     Account line (account code, name)
-    holding section:
+    holding section (0 or 1 holding section):
         ...
-    cash section:
+    cash section (0 or 1 cash section):
         ...
 
     (2) Special case:
     Account line (account code, name)
-    No data for this account
+    No data for this account (second line)
 
     """
     emptyAccount = lambda L: True if len(L) > 0 and L[0] == 'No Data for this Account' \
                                 else False
 
     if emptyAccount(lines[1]):
-        return ('', [], [])
+        return ('', [])
 
 
     cashSection = lambda L: True if len(L) > 0 and L[0] == 'Branch Code' \
@@ -132,7 +137,6 @@ def readPosition(lines):
 
     sections = itemGroup(emptyLine, lines)
     headers = readHeaders(pop(sections))   # first section is the header
-    emptyHeader = lambda pair: emptyString(pair[0])
     return map(partial(position, headers), sections)
 
 
@@ -161,41 +165,174 @@ def position(headers, lines):
 
     line: a list of values
     """
+    emptyHeader = lambda pair: emptyString(pair[0])
+    toDictionary = lambda headers, values: dict(filterfalse(emptyHeader
+                                                           , zip(headers, values)))
+
     return toDictionary(headers
                        , reduce(add, filterfalse(emptyLine, lines), []))
 
 
 
-def toDictionary(headers, values):
-    emptyHeader = lambda pair: emptyString(pair[0])
-    return dict(filterfalse(emptyHeader, zip(headers, values)))
+def genevaPosition(date, accountInfo):
+    """
+    [Sring] date, [Tuple] accountInfo => [Iterable] Geneva Position
+
+    Where accountInfo is the outcome of the account() function, consisting of
+    (accountCode, positions)
+
+    Where a Geneva position is either a holding position or cash position
+    depending on the input position
+    """
+    accountCode, positions = accountInfo
+
+    def toGenevaPosition(position):
+        if 'Security ID' in position:
+            return genevaHolding(getPortId(accountCode), date, position)
+        else:
+            return genevaCash(getPortId(accountCode), date, position)
+
+
+    return map(toGenevaPosition, positions)
 
 
 
-def emptyString(x):
-    if isinstance(x, str) and x.strip() == '':
-        return True
+def genevaHolding(portId, date, holding):
+    """
+    [String] portId, [String] date, [Dictionary] holding => 
+        [Dictionary] gPosition
+    
+    A Geneva position is a dictionary object that has the following
+    keys:
+
+    portfolio|custodian|date|geneva_investment_id|ISIN|bloomberg_figi|name
+    |currency|quantity
+    
+    """
+    genevaPos = {}
+    genevaPos['portfolio'] = portId
+    genevaPos['custodian'] = getCustodian()
+    genevaPos['date'] = date
+    genevaPos['name'] = holding['Security Name']
+    genevaPos['currency'] = getCurrency(holding['Security Name'], holding['Security ID'].strip())
+    genevaPos['quantity'] = holding['Total Units']
+    (genevaPos['geneva_investment_id'], genevaPos['ISIN'], genevaPos['bloomberg_figi']) \
+                = getSecurityId(portId, holding['ISIN'].strip(), holding['Security ID'].strip())
+    
+    return genevaPos
+
+
+
+def genevaCash(portId, date, cash):
+    """
+    [String] portId, [String] date, [Dictionary] cash => 
+        [Dictionary] gCash
+
+    A Geneva cash position is a dictionary object that has the following
+    keys:
+
+    portfolio|custodian|date|currency|balance
+    
+    """
+    genevaCash = {}
+    genevaCash['portfolio'] = portId
+    genevaCash['custodian'] = getCustodian()
+    genevaCash['date'] = date
+    genevaCash['currency'] = cash['Local CCY']
+    genevaCash['balance'] = cash['Closing Cash Balance']
+    
+    return genevaCash
+
+
+
+def getSecurityId(portId, isin, securityId):
+    """
+    [String] portId, [String] isin, [String] securityId =>
+        [Tuple] (geneva_investment_id, isin, bloomberg_figi)
+    """
+    if emptyString(isin):
+        security_id_type = 'JPM'
+        security_id = securityId
     else:
-        return False
+        security_id_type = 'ISIN'
+        security_id = isin
+
+    investment_ids = get_investment_Ids(portId, security_id_type, security_id)
+
+    # For portfolio 12404, give special treatment for this position:
+    # 
+    # SINO-OCEAN GROUP HOLDING LTD COMMON STOCK HKD 0
+    # with isin = 'HK3377040226'. 
+    # 
+    # Although it is a common stock, however, it is treated as private security 
+    # in Geneva due to special accounting treatment.
+    # 
+    if portId == '12404' and investment_ids == ('', 'HK3377040226', ''):
+        investment_ids = ('SINO OCEAN LAND_DUMMY', '', '')
+
+    return investment_ids
 
 
 
-def emptyLine(line):
+def getCurrency(name, securityId):
     """
-    [List] line => [Bool] is all the cell empty
+    [String] name => [String] currency
     """
-    return all(emptyString(x) for x in line)
+    return currencyFromName(name) or lookup_investment_currency('JPM', securityId)
 
 
 
-def portfolioId(accountCode):
+def currencyFromName(name):
     """
-    [String] accountCode => [String] portfolioId
+    Extract the currency from the security name
 
-    Map account code from JPM to portfolio id in Geneva
+    [String] name => [String] currency
+
+    security name looks like:
+
+    HUI XIAN REAL ESTATE INVESTMENT TRUST REIT CNY
+
+    PICC PROPERTY & CASUALTY CO LTD COMMON STOCK HKD 1
+
+    1MDB ENERGY LTD NOTES FIXED 5.99% 11/MAY/2022 USD 100000
     """
-    return 'TestPortId' #FIXME
+    isCurrency = lambda x: x in ['HKD', 'USD', 'CNY', 'SGD', 'JPY', 'EUR']
+    return firstOf(isCurrency, name.split()[-2:])
 
+
+
+def getCustodian():
+    return 'JPM'
+
+
+
+def getPortId(accountCode):
+    """
+    Map the account code of JP Morgan to the portfolio id in Geneva.
+    """
+    p_map = {
+        # China Life overseas accounts
+        '48029': '11490',
+        '48089': '12341',
+        '48090': '12298',
+        '48195': '12548',  # CLO trustee's bond fund
+        '53412': '12857',
+        '53413': '12856',
+        '48194': '12726',
+
+        # China Life ListCo accounts
+        'AFU34': '12404',
+        'AFU35': '12307',
+        'BBK32': '12094',
+        'AFU37': '12086',
+        'AHS61': '12087'
+    }
+
+    try:
+        return p_map[accountCode]
+    except KeyError:
+        logger.error('getPortId(): invalid account code {0}'.format(accountCode))
+        raise ValueError
 
 
 
@@ -204,13 +341,10 @@ if __name__ == '__main__':
     inputFile = join(getCurrentDirectory(), 'samples', 'statement01.xls')
 
     lines = worksheetToLines(open_workbook(inputFile).sheet_by_index(0))
-    # for x in readPosition(islice(lines, 8, 22)):
+    # accountCode, positions = account(list(islice(lines, 7, 201)))
+    # print(accountCode)
+    # for x in positions:
     #     print(x)
 
-    # for x in readPosition(islice(lines, 194, 201)):
-    #     print(x)
-
-    accountCode, positions = account(list(islice(lines, 7, 201)))
-    print(accountCode)
-    for x in positions:
+    for x in genevaPosition('2016-06-07', account(list(islice(lines, 7, 201)))):
         print(x)
